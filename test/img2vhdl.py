201f787f3acb0d1d32c38febddcbed9fd9411645
t.k <clock126@126.com>
2014-04-30 02:01:06 -0400

    bitmap2vhdl script added.

---
+++ test/img2vhdl.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from PIL import Image
+im = Image.open ("1.png")
+rgb_im = im.convert('RGB')
+pix = rgb_im.load()
+w, h = rgb_im.size
+
+print '('
+for y in range(h):
+	print '  (',
+	for x in range(w):
+		r = int(pix[x, y][0]/255.0 * 7 + 0.5)
+		g = int(pix[x, y][1]/255.0 * 7 + 0.5)
+		b = int(pix[x, y][2]/255.0 * 3 + 0.5)
+
+		r = "{0:03b}".format(r)
+		g = "{0:03b}".format(g)
+		b = "{0:02b}".format(b)
+		tmp = '%s%s%s' % (r,g,b)
+		print int(tmp, 2),
+		if x < w - 1:
+			print ',',
+	if y < h - 1:
+		print '),'
+	else: 
+		print ')'
+print ')'
